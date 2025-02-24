95f31ba99b9074929a67098ec1ae75b04f56386e
Minh Tính Lê <leminhtinh.official@outlook.com>
2025-02-24 15:22:59 +0700

    Abp blob: Add service convert

---
+++ PawsomePets/src/PawsomePets.Domain/AbpBlobContainers/FileServices/FileService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace nRetailApp.Services.FileService;
+
+public class FileService : IFileService
+{
+    public IFormFile ConvertBase64ToIFormFile(string base64)
+    {
+        var _base64 = base64.Trim();
+
+        if (_base64.Length % 4 == 0 && Regex.IsMatch(_base64, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None))
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var stream = new MemoryStream(bytes);
+            IFormFile file = new FormFile(stream, 0, stream.Length, null, Guid.NewGuid() + ".jpg")
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = Image.Jpeg
+            };
+            return file;
+        }
+
+        throw new InvalidOperationException("Base64 not match");
+    }
+}
---
+++ PawsomePets/src/PawsomePets.Domain/AbpBlobContainers/FileServices/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace nRetailApp.Services.FileService;
+
+public interface IFileService
+{
+    IFormFile ConvertBase64ToIFormFile(string base64);
+}
